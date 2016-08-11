cc6377bc815498bac591efab947de376bd3a9f33
Andrzej <ferexio@users.noreply.github.com>
2016-08-12 01:25:42 +0200

    Edit Calc.c to newest version by me.

--- calc.c
+++ calc.c
@@ -9,13 +9,30 @@ scanf("%i", &b);
 if (a < 1 || b < 1)
 {printf("Error, zle dane !\n");}
 else{
-printf("Kalkulator by Ferex Blog 2013-2016\n");
-printf("Wynik dodawania to: %i\n", a+b);
-printf("Wynik odejmowania to: %i\n", a-b);
-printf("Wynik modulo to: %i\n", a%b);
-printf("Wynik mnozenia to: %i\n", a*b);
-printf("Wynik dzielenia to: %i\n", a/b);
-printf("Reszta funkcji na dzien dzisiejszy jest niedostepna :)\n");
+calc:
+printf("Podaj mi co chcesz zrobic:\n");
+printf("1) Dodać\n");
+printf("2) Odjąć\n");
+printf("3) Pomnożyć\n");
+printf("4) Odjąć\n");
+printf("5) Obliczyć modulo\n");
+printf("6) Zakończyć program\n");
+printf("Twój wybór to: ");
+scanf("%i", &c);
+if ( c < 1 || c > 6 )
+{goto calc;}
+else if ( c == 1)
+{printf("Wynik dodawania to: %i\n", a+b);}
+else if ( c == 2)
+{printf("Wynik odejmowania to: %i\n", a-b);}
+else if ( c == 3)
+{printf("Wynik mnożenia to: %i\n", a*b);}
+else if ( c == 4)
+{printf("Wynik dzielenia to: %i\n", a/b);}
+else if ( c == 5)
+{printf("Wynik modulo to: %i\n", a%b);}
+else if ( c == 6)
+{return 0;}
 }
 return 0;
 }
