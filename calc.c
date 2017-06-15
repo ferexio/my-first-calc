bc30deec846e0cc9e5f395412059e4ac03ad9446
FX <ferexio@users.noreply.github.com>
2017-06-15 12:37:54 +0200

    Create calc.c

--- calc.c
+++ calc.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-int a,b;
+int a,b,c;
 printf("Podaj a: ");
 scanf("%i", &a);
 printf("Podaj b: ");
@@ -9,7 +9,9 @@ scanf("%i", &b);
 if (a < 1 || b < 1)
 {printf("Error, zle dane !\n");}
 else{
-calc:
+c = 0;
+while ( c < 1 || c > 6 )
+{
 printf("Podaj mi co chcesz zrobic:\n");
 printf("1) Dodać\n");
 printf("2) Odjąć\n");
@@ -19,9 +21,8 @@ printf("5) Obliczyć modulo\n");
 printf("6) Zakończyć program\n");
 printf("Twój wybór to: ");
 scanf("%i", &c);
-if ( c < 1 || c > 6 )
-{goto calc;}
-else if ( c == 1)
+}
+if ( c == 1)
 {printf("Wynik dodawania to: %i\n", a+b);}
 else if ( c == 2)
 {printf("Wynik odejmowania to: %i\n", a-b);}
@@ -33,6 +34,7 @@ else if ( c == 5)
 {printf("Wynik modulo to: %i\n", a%b);}
 else if ( c == 6)
 {return 0;}
+
 }
 return 0;
 }
